54d8a2e97367e1d4f8526774ebbc2058f3f9d62f
sucgul <sucgul@gmail.com>
2026-05-08 10:51:59 +0300

    Add SQL for generating random numbers
    Added SQL queries to generate random numbers within specified ranges.

---
+++ creating_random_numbers.sql
@@ -0,0 +1,11 @@
+--MAIN FORMULAS is that; SELECT FLOOR(RANDOM() * (high - low + 1) + low)::int
+
+--CREATING [1,99] numbers
+--SELECT FLOOR (RANDOM() *(99-1+1) + 1)::int
+SELECT FLOOR (RANDOM() *(99-1+1) + 1)::int
+
+
+--CREATING [4,19] numbers
+--SELECT FLOOR (RANDOM() * (19-4+1) +4)::int
+SELECT FLOOR (RANDOM() * (16) +4)::int
+
